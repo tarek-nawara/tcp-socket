a2c02b68131353cd2be35aa37a6b3099e24c44af
Tarek <tarek.nawara@gmail.com>
2017-11-15 23:56:19 +0200

    initial commit

---
+++ utils/socket_utils.cpp
@@ -0,0 +1,7 @@
+/**
+ * File: socket_utils.cpp
+ * Description: implementation for socket wrapper functions
+ * Created Date: 2017-11-15
+ */
+
+#include "socket_utils.h"
---
+++ utils/socket_utils.h
@@ -0,0 +1,11 @@
+/**
+ * File: socket_utils.h
+ * Description: socket wrapper methods
+ * Created Date: 2017-11-15
+ */
+
+#ifndef SOCKET_PROGRAMMING_SOCKET_UTILS_H
+#define SOCKET_PROGRAMMING_SOCKET_UTILS_H
+
+
+#endif //SOCKET_PROGRAMMING_SOCKET_UTILS_H
