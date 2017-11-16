d9c3b5eec2ec336b0a631d5682b75714d444b06b
Tarek <tarek.nawara@gmail.com>
2017-11-16 03:33:35 +0200

    changed project structure

---
+++ tcp_server/TcpServer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tarek on 11/16/17.
+//
+
+#include "TcpServer.h"
---
+++ tcp_server/TcpServer.h
@@ -0,0 +1,14 @@
+//
+// Created by tarek on 11/16/17.
+//
+
+#ifndef TCP_SERVER_TCPSERVER_H
+#define TCP_SERVER_TCPSERVER_H
+
+
+class TcpServer {
+
+};
+
+
+#endif //TCP_SERVER_TCPSERVER_H
